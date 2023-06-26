4b905e57b201b2b0dc75c556c3468cc100f13b5a
finall00 <vvcruzpelissoni08@gmail.com>
2023-06-26 14:41:30 -0300

    colocando login como publico

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,7 +7,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-// import { IsPublic } from './decorators/is-public.decorator';
+import { IsPublic } from './decorators/is-public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/AuthRequest';
 
@@ -15,7 +15,8 @@ import { AuthRequest } from './models/AuthRequest';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  //   @IsPublic()
+  @IsPublic()
+  
   @UseGuards(LocalAuthGuard)
   @Post('login')
   @HttpCode(HttpStatus.OK)
